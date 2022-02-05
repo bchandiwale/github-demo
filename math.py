55e2fed4f084d5b29bde3ed9928dbe127604b265
bchandiwale <86349812+bchandiwale@users.noreply.github.com>
2022-02-05 23:41:51 +0530

    add math

---
+++ math.py
@@ -0,0 +1,15 @@
+#add implementation
+def add(x, y):
+    return x+y
+#subtract implementation
+def subtract (x, y):
+    pass
+#multiply implementation
+def multiply (x, y):
+    pass
+#divide implementation
+def divide (x, y):
+	if y==0:
+		return a
+	else
+		return x/y
